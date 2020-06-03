b69e0f0e0ff123c9f7855be1e20160eaaf79bdee
Huzaifa Ehsan <huzaifaehsan_2023@depauw.edu>
2020-06-03 00:26:27 -0400

    Made the Start page for the game

---
+++ Assets/Scripts/Republic.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Republic: MonoBehaviour
+{
+    private Slider republicSlider;
+   
+    public float FillSpeed = 0.5f;
+    private float targetProgress = 0;
+
+    private void Awake()
+    {
+        republicSlider = gameObject.GetComponent<Slider>();
+        }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        IncrementProgress(0.5f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (republicSlider.value < targetProgress)
+            republicSlider.value += FillSpeed * Time.deltaTime;
+    }
+
+    public void IncrementProgress(float newProgress)
+    {
+        targetProgress = republicSlider.value + newProgress;
+    }
+}
---
+++ Assets/Scripts/ShowValueScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShowValueScript : MonoBehaviour
+{
+    Text percentageText;
+
+    void Start()
+    {
+        percentageText = GetComponent<Text>();
+    }
+
+    public void TextUpdate(float value)
+    {
+        percentageText.text = Mathf.RoundToInt(value * 100) + "%";
+    }
+}
