f0c4db67b660a679294d8d8888a1170d1fa818b7
Hamza Ehsan <hamzaehsan@Hamzas-MacBook-Pro.local>
2020-06-03 03:14:00 -0500

    Fixed screen ratios

---
+++ Temp/il2cppOutput/il2cppOutput/Assembly-CSharp_CodeGen.c
@@ -0,0 +1,103 @@
+﻿#include "il2cpp-config.h"
+
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+IL2CPP_EXTERN_C_BEGIN
+IL2CPP_EXTERN_C_END
+
+
+
+
+// 0x00000001 System.Void Democratic::Awake()
+extern void Democratic_Awake_mE94A28E40170AE9BB3676FA48B7AC78D4A730773 ();
+// 0x00000002 System.Void Democratic::Start()
+extern void Democratic_Start_m49D2A1C868558CE4AA3C18696655D4A309FC28DC ();
+// 0x00000003 System.Void Democratic::Update()
+extern void Democratic_Update_m87689FE2D08DC7989D108277EC867EBF13772627 ();
+// 0x00000004 System.Void Democratic::IncrementProgress(System.Single)
+extern void Democratic_IncrementProgress_m60C3C2E62E8C91CA726D62F9D12A291FE6B75DD3 ();
+// 0x00000005 System.Void Democratic::.ctor()
+extern void Democratic__ctor_m97527A8233B99D1CC43E316CBF5A6C32AB52D5A5 ();
+// 0x00000006 System.Void MainMenu::PlayGame()
+extern void MainMenu_PlayGame_m4CD3D61E23D84AD1A018C84D561EAE39ED2D76F7 ();
+// 0x00000007 System.Void MainMenu::.ctor()
+extern void MainMenu__ctor_mF17B753D99BD98B88E949A5B9CA53892E19A6CD5 ();
+// 0x00000008 System.Void Republic::Awake()
+extern void Republic_Awake_m1B96183D1031C878AA2834364980917A58C2B517 ();
+// 0x00000009 System.Void Republic::Start()
+extern void Republic_Start_m90A8CDB161D2B2C5CAE0A730A0B23F3709161847 ();
+// 0x0000000A System.Void Republic::Update()
+extern void Republic_Update_mA1F969907648CE5C93D7A154CB779F425E12126E ();
+// 0x0000000B System.Void Republic::IncrementProgress(System.Single)
+extern void Republic_IncrementProgress_m9B5BEF259E4FE90A75CDCD2F582BBD6F239F58EE ();
+// 0x0000000C System.Void Republic::.ctor()
+extern void Republic__ctor_m973ACE60856ED230C3AA78471646DA7726D4B42F ();
+// 0x0000000D System.Void ShowValueScript::Start()
+extern void ShowValueScript_Start_m7A818679958B3E28A6714ABDF562CB9EE476D233 ();
+// 0x0000000E System.Void ShowValueScript::TextUpdate(System.Single)
+extern void ShowValueScript_TextUpdate_m859FF6D637729B4A86F43E9C1F641F1EFF757BAD ();
+// 0x0000000F System.Void ShowValueScript::.ctor()
+extern void ShowValueScript__ctor_m38DB6347ECF001681AB93FA4C25738BD15EDC3FC ();
+static Il2CppMethodPointer s_methodPointers[15] = 
+{
+	Democratic_Awake_mE94A28E40170AE9BB3676FA48B7AC78D4A730773,
+	Democratic_Start_m49D2A1C868558CE4AA3C18696655D4A309FC28DC,
+	Democratic_Update_m87689FE2D08DC7989D108277EC867EBF13772627,
+	Democratic_IncrementProgress_m60C3C2E62E8C91CA726D62F9D12A291FE6B75DD3,
+	Democratic__ctor_m97527A8233B99D1CC43E316CBF5A6C32AB52D5A5,
+	MainMenu_PlayGame_m4CD3D61E23D84AD1A018C84D561EAE39ED2D76F7,
+	MainMenu__ctor_mF17B753D99BD98B88E949A5B9CA53892E19A6CD5,
+	Republic_Awake_m1B96183D1031C878AA2834364980917A58C2B517,
+	Republic_Start_m90A8CDB161D2B2C5CAE0A730A0B23F3709161847,
+	Republic_Update_mA1F969907648CE5C93D7A154CB779F425E12126E,
+	Republic_IncrementProgress_m9B5BEF259E4FE90A75CDCD2F582BBD6F239F58EE,
+	Republic__ctor_m973ACE60856ED230C3AA78471646DA7726D4B42F,
+	ShowValueScript_Start_m7A818679958B3E28A6714ABDF562CB9EE476D233,
+	ShowValueScript_TextUpdate_m859FF6D637729B4A86F43E9C1F641F1EFF757BAD,
+	ShowValueScript__ctor_m38DB6347ECF001681AB93FA4C25738BD15EDC3FC,
+};
+static const int32_t s_InvokerIndices[15] = 
+{
+	23,
+	23,
+	23,
+	289,
+	23,
+	23,
+	23,
+	23,
+	23,
+	23,
+	289,
+	23,
+	23,
+	289,
+	23,
+};
+extern const Il2CppCodeGenModule g_AssemblyU2DCSharpCodeGenModule;
+const Il2CppCodeGenModule g_AssemblyU2DCSharpCodeGenModule = 
+{
+	"Assembly-CSharp.dll",
+	15,
+	s_methodPointers,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+};
